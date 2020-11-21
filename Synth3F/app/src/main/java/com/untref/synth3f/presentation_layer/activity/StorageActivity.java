0e1d4d98e3d33c7c9cb71759c91faf14773b5a71
gabrielbosio <gabrielbosio95@gmail.com>
2020-11-21 00:59:56 -0300

    Diálogo de aviso de sobrescritura

--- Synth3F/app/src/main/java/com/untref/synth3f/presentation_layer/activity/StorageActivity.java
+++ Synth3F/app/src/main/java/com/untref/synth3f/presentation_layer/activity/StorageActivity.java
@@ -1,11 +1,14 @@
 package com.untref.synth3f.presentation_layer.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -21,6 +24,7 @@ import static com.untref.synth3f.domain_layer.serializers.FileManager.getFilenam
 public class StorageActivity extends AppCompatActivity {
 
     private boolean closeApp;
+    private boolean saveMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +44,12 @@ public class StorageActivity extends AppCompatActivity {
     }
 
     private void setMode(int mode) {
-        boolean saveMode = mode == PatchGraphFragment.REQUEST_SAVE;
+        saveMode = mode == PatchGraphFragment.REQUEST_SAVE;
         TextView inputFileNameView = findViewById(R.id.inputFilename);
         inputFileNameView.setEnabled(saveMode);
         inputFileNameView.setHint(R.string.save_hint);
+        Button buttonSave = findViewById(R.id.buttonSave);
+        buttonSave.setText(getString(saveMode ? R.string.dialog_save : R.string.dialog_load));
     }
 
     private void createClickEvents() {
@@ -57,14 +63,12 @@ public class StorageActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Intent returnIntent = new Intent();
                         String filename = ((EditText) findViewById(R.id.inputFilename)).getText().toString();
-                        if (!isOverwriting(filename)) {
-                            returnIntent.putExtra("filename", filename);
-                            returnIntent.putExtra("closeApp", closeApp);
-
-                            setResult(PatchGraphFragment.RESULT_OK, returnIntent);
-                            finish();
+                        List<String> filenameList = getFilenameList(getBaseContext());
+                        if (saveMode && filenameList.contains(filename)) {
+                            handleFileOverwriting(filename);
+                        } else {
+                            returnToFragment(filename);
                         }
                     }
                 });
@@ -80,12 +84,36 @@ public class StorageActivity extends AppCompatActivity {
                 });
     }
 
-    private boolean isOverwriting(String newFilename) {
-        List<String> filenameList = getFilenameList(getBaseContext());
-        boolean filenameAlreadyExists = filenameList.contains(newFilename);
-        if (filenameAlreadyExists) {
-            // TODO: Overwriting dialog
-        }
-        return filenameAlreadyExists;
+    private void handleFileOverwriting(final String filename) {
+        int dialogStyle = R.style.Theme_AppCompat_Dialog_Alert;
+        AlertDialog dialog = new AlertDialog.Builder(this, dialogStyle).create();
+        dialog.setTitle(getString(R.string.overwrite_dialog_title));
+        dialog.setMessage(getString(R.string.overwrite_dialog_message));
+        dialog.setButton(android.app.AlertDialog.BUTTON_NEGATIVE,
+                getString(R.string.dialog_cancel),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        dialog.setButton(android.app.AlertDialog.BUTTON_POSITIVE,
+                getString(R.string.dialog_accept),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        returnToFragment(filename);
+                    }
+                });
+        dialog.show();
+    }
+
+    private void returnToFragment(String filename) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("filename", filename);
+        returnIntent.putExtra("closeApp", closeApp);
+
+        setResult(PatchGraphFragment.RESULT_OK, returnIntent);
+        finish();
     }
 }
