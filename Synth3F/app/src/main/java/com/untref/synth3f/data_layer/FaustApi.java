41346d358f4ce85934bb852c3292a2ed6f2944bb
gabrielbosio <gabrielbosio95@gmail.com>
2020-12-05 20:56:48 -0300

    Unificada convención en capa de datos

--- Synth3F/app/src/main/java/com/untref/synth3f/data_layer/FaustApi.java
+++ Synth3F/app/src/main/java/com/untref/synth3f/data_layer/FaustApi.java
@@ -11,17 +11,22 @@ public class FaustApi {
 
     public static native void disposeFaustApi(long android_graph_adapter_pointer);
 
-    public static native void addPatchFaustApi(long android_graph_adapter_pointer, String code, int id);
+    public static native void addPatchFaustApi(long android_graph_adapter_pointer, String code,
+                                               int id);
 
     public static native void removePatchFaustApi(long android_graph_adapter_pointer, int id);
 
-    public static native void setValueFaustApi(long android_graph_adapter_pointer, String name, float value);
+    public static native void setValueFaustApi(long android_graph_adapter_pointer, String name,
+                                               float value);
 
-    public static native void connectFaustApi(long android_graph_adapter_pointer, int source, int outlet, int target, int inlet);
+    public static native void connectFaustApi(long android_graph_adapter_pointer, int source,
+                                              int outlet, int target, int inlet);
 
-    public static native void disconnectFaustApi(long android_graph_adapter_pointer, int source, int outlet, int target, int inlet);
+    public static native void disconnectFaustApi(long android_graph_adapter_pointer, int source,
+                                                 int outlet, int target, int inlet);
 
-    public static native void processFaustApi(long android_graph_adapter_pointer, float[] jdata, int channels);
+    public static native void processFaustApi(long android_graph_adapter_pointer, float[] jdata,
+                                              int channels);
 
     public static native void startAudioFaustApi(long android_graph_adapter_pointer);
 
