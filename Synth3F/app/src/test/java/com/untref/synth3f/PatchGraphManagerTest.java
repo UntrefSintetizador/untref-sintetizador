a701f7223f0f049eaef3a3fe65d7d3eeeeafacfb
gabrielbosio <gabrielbosio95@gmail.com>
2020-12-05 22:15:36 -0300

    Agregado test de conexión de dos patches

--- Synth3F/app/src/test/java/com/untref/synth3f/PatchGraphManagerTest.java
+++ Synth3F/app/src/test/java/com/untref/synth3f/PatchGraphManagerTest.java
@@ -1,6 +1,7 @@
 package com.untref.synth3f;
 
 import com.untref.synth3f.domain_layer.helpers.PatchGraphManager;
+import com.untref.synth3f.entities.Connection;
 import com.untref.synth3f.entities.DACPatch;
 import com.untref.synth3f.entities.Patch;
 import com.untref.synth3f.entities.VCFPatch;
@@ -8,7 +9,8 @@ import com.untref.synth3f.entities.VCOPatch;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PatchGraphManagerTest {
@@ -27,5 +29,26 @@ public class PatchGraphManagerTest {
         assertThat(firstPatch.getId(), is(not(secondPatch.getId())));
         assertThat(secondPatch.getId(), is(not(thirdPatch.getId())));
         assertThat(thirdPatch.getId(), is(not(firstPatch.getId())));
+    }
+
+    @Test
+    public void connectTwoPatches() {
+        Patch sourcePatch = new VCOPatch();
+        Patch destinyPatch = new DACPatch();
+        int outletId = 0;
+        int inletId = 1;
+        PatchGraphManager patchGraphManager = new PatchGraphManager();
+        patchGraphManager.addPatch(sourcePatch);
+        patchGraphManager.addPatch(destinyPatch);
+
+        patchGraphManager.connect(sourcePatch.getId(), outletId, destinyPatch.getId(), inletId);
+
+        Connection sourceOutput = sourcePatch.getOutputConnections().get(0);
+        Connection destinyInput = destinyPatch.getInputConnections().get(0);
+        assertThat(sourceOutput, is(destinyInput));
+        assertThat(sourceOutput.getSourceOutlet(), is(outletId));
+        assertThat(sourceOutput.getSourcePatch(), is(sourcePatch.getId()));
+        assertThat(sourceOutput.getTargetInlet(), is(inletId));
+        assertThat(sourceOutput.getTargetPatch(), is(destinyPatch.getId()));
     }
 }
